06cc093026204f4cb5979d32156eef1bbce94ac6
Avi Goodman <avi.goodman4@gmail.com>
2026-02-23 15:03:27 -0500

    Fix port for Railway deployment

--- server.js
+++ server.js
@@ -591,7 +591,7 @@ socket.on('nextRound', () => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Dink server running at http://localhost:${PORT}`);
+  console.log(`Dink server running on port ${PORT}`);
 });
